0e83151c15cb4221f6188d1c382346e9ad42effa
Nandika Jeevantha <jeevanthanandika@gmail.com>
2020-11-05 20:43:31 +0530

    upgrade

--- src/components/counter/counter.component.jsx
+++ src/components/counter/counter.component.jsx
@@ -6,20 +6,43 @@ class Counter extends React.Component {
     constructor() {
         super();
         this.state = {
-            counter: 0
+            count: 0
         }
     }
 
     incrementCount = () => {
-        alert('ndnfidnf')
+        this.setState({
+            count: this.state.count + 1
+        })
+
+    }
+
+    decrementCount = () => {
+        this.setState({
+            count: this.state.count - 1
+        })
+    }
+
+    renderCountValue = () => {
+        if (this.state.count == 0) {
+            return 'Zero'
+        }
+        return this.state.count
+    }
+
+    changeCountColor = () => {
+        if (this.state.count == 0) {
+            return "badge badge-pill badge-warning p-2"
+        }
+        return "badge badge-pill badge-primary p-2"
     }
 
     render() {
         return (
             <div className="counterdiv">
-                <span class="badge badge-pill badge-primary p-2">10</span>
+                <span class={this.changeCountColor()}>{this.renderCountValue()}</span>
                 <button className="btn btn-primary" onClick={this.incrementCount}>Increment</button>
-                <button className="btn btn-warning">Decrement</button>
+                <button className="btn btn-warning" onClick={this.decrementCount}>Decrement</button>
                 <button className="btn btn-danger">Delete</button>
             </div>
         )
