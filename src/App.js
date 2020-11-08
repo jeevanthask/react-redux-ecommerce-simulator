8e33a0beb3df413eb4d4b7cc91a14ab7aa61bfd8
Nandika Jeevantha <jeevanthanandika@gmail.com>
2020-11-08 18:55:25 +0530

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Counter from "./components/counter/counter.component";
 import Navbar from "./components/navbar/navbar.component";
+import Counters from "./components/counters/counters.component";
+
 
 class App extends React.Component {
     render() {
@@ -14,7 +16,7 @@ class App extends React.Component {
 
                 <div className="row">
                     <div className="col-md-12">
-                        <Counter/>
+                        <Counters/>
                     </div>
                 </div>
 
