a5dfc8446c144a5539a49d45f4c024ea739fc194
Nandika Jeevantha <jeevanthanandika@gmail.com>
2020-11-08 18:52:26 +0530

    upgrade

---
+++ src/components/counters/counters.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './counters.component.css'
+
+class Counters extends React.Component {
+    constructor() {
+        super();
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div>
+                ndcnduc
+            </div>
+        );
+    }
+}
+
+export default Counters
