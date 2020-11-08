8cf0caf8ec1213c8f08655d603db24b5252931db
Nandika Jeevantha <jeevanthanandika@gmail.com>
2020-11-08 19:24:56 +0530

    upgrade

--- src/components/counters/counters.component.jsx
+++ src/components/counters/counters.component.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import './counters.component.css'
+import Counter from "../counter/counter.component";
 
 class Counters extends React.Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            counters: [
+                {id: 1, count: 0},
+                {id: 2, count: 0},
+                {id: 3, count: 0}
+            ]
+        }
     }
 
     render() {
         return (
             <div>
-                ndcnduc
+                {
+                    this.state.counters.map(count => <Counter/>)
+                }
             </div>
         );
     }
